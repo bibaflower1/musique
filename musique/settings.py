5697e697e7dbb1d6c563f3fa5b91100cf92a4fab
nabila amdouni <amdouni.nabila11@gmail.com>
2018-10-13 22:16:02 +0200

    changing allowed host

--- musique/settings.py
+++ musique/settings.py
@@ -29,8 +29,8 @@ if os.environ.get('ENV') == 'PRODUCTION':
 else:
     DEBUG = True
 
-ALLOWED_HOSTS = []
-#ALLOWED_HOSTS = ['moved.herokuapp.com']
+#ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['moved.herokuapp.com']
 
 # Application definition
 
